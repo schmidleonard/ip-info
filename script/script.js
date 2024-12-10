efffd41a20957e498dc2884708496d162f40e699
Leonard Schmid <leonardschmid96@gmail.com>
2024-12-10 16:02:52 +0100

    add functions for whatisdns

--- script/script.js
+++ script/script.js
@@ -1,26 +1,60 @@
 
-
+// fetch ip data
 function searchIpInfo(){
-    console.log('search Called')
+    console.log('ip api Called');
 
 
     let searchStr = document.getElementById('searchInput').value;
     console.log("searched for " + searchStr);
 
-    fetch('http://ip-api.com/json/' + searchStr).then(res => res.json()).then(apiSearchJson => {
-        createResult(apiSearchJson);
+    fetch('http://ip-api.com/json/' + searchStr).then(res => res.json()).then(ipSearchJson => {
+        createIpResult(ipSearchJson);
     })
 }
 
 
-
-function createResult(apiJson){
+// create IP Result
+function createIpResult(ipSearchJson){
 
     document.getElementById('createContent').innerHTML = '';
 
 
     let ipText = document.createElement('p');
-    ipText.innerHTML = apiJson.query;
+    ipText.innerHTML = ipSearchJson.query;
 
     document.getElementById('createContent').appendChild(ipText);
+}
+
+// fetch dns data
+function searchDns(){
+    console.log('dns api called');
+
+    fetch('http://edns.ip-api.com/json').then(res => res.json()).then(dnsSearchJson => {
+        createDnsResult(dnsSearchJson);
+    })
+}
+
+function createDnsResult(dnsSearchJson){
+    //clear Div
+    document.getElementById('createContentDns').innerHTML = '';
+
+    // split information from Json "geo" into isp and location
+    let inputParts = dnsSearchJson.dns.geo.split('-');
+
+    let geo = inputParts[0];
+    let isp = inputParts[1];
+
+
+
+    let dnsIP = document.createElement('p');
+    dnsIP.innerHTML = 'Dein aktueller DNS-Server hat folgende IP-Adresse: ' + dnsSearchJson.dns.ip;
+    let ispName = document.createElement('p');
+    ispName.innerHTML = 'Dein ISP ist ' + isp;
+
+    let dnsGeo = document.createElement('p');
+    dnsGeo.innerHTML = 'Der DNS-Server steht in ' + geo;
+
+    document.getElementById('createContentDns').appendChild(dnsIP);
+    document.getElementById('createContentDns').appendChild(ispName);
+    document.getElementById('createContentDns').appendChild(dnsGeo);
 }
