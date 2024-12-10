ff95e70df132940b38081e10555804f45a680563
Leonard Schmid <leonardschmid96@gmail.com>
2024-12-10 09:20:46 +0100

    delete function for own ip

--- script/script.js
+++ script/script.js
@@ -12,15 +12,7 @@ function searchIpInfo(){
     })
 }
 
-function searchMyIp(){
-    console.log('my Ip Called')
 
-    console.log("searched for ");
-
-    fetch('http://ip-api.com/json/').then(res => res.json()).then(apiSearchJson => {
-        createResult(apiSearchJson);
-    })
-}
 
 function createResult(apiJson){
 
